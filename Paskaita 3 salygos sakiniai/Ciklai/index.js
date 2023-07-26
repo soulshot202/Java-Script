c23931a97514fdca982efbd21b738b5475d4fa0f
Vytautas Berzvinskas <misionierius@gmail.com>
2023-07-26 19:35:49 +0300

    3 paskaita

---
+++ Paskaita 3 salygos sakiniai/Ciklai/index.js
@@ -0,0 +1,18 @@
+let num = 10;
+while(num < 17){
+    console.log(num);
+    num++;
+}
+let num1 = 10;
+do{
+    console.log(num1);
+    num1++;
+}
+while (num1 >17);
+
+// ----------
+
+
+for(let i = 0; i < 10; ++i){
+    console.log(i);
+}
