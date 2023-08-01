010cafa798d09128e7d809bf8b4af53b92d98521
Vytautas Berzvinskas <misionierius@gmail.com>
2023-08-01 19:44:26 +0300

    paskaita 6

--- Paskaita 6 formu valdymas/index.js
+++ Paskaita 6 formu valdymas/index.js
@@ -6,8 +6,41 @@ const commentInput = document.querySelector("input[type=text]");
 const driversLicenseCheckBoxInput = document.querySelector(
   "input[type=checkbox]"
 );
+const helloBtn = document.querySelector("#helloBtn");
 const makeSelect = document.querySelector("#make");
+const h1 = document.querySelector("h1");
+const passwordInput = document.querySelector("[type=password]");
+
 driversLicenseCheckBoxInput.addEventListener("change", handleCheckBoxChange);
+helloBtn.addEventListener("click", helloHandler);
+passwordInput.addEventListener("input", passwordChangeHandler);
+
+function passwordChangeHandler(e) {
+  const regex =
+    /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*])[a-zA-Z\d!@#$%^&*]{8,}$/;
+  const password = e.target.value;
+  const test = regex.test(password);
+
+  if (test) {
+    passwordInput.classList.add(`good`);
+    passwordInput.classList.remove(`bad`);
+  } else {
+    passwordInput.classList.add(`bad`);
+    passwordInput.classList.remove(`good`);
+  }
+  // !password tas pats kas password === ""
+  if (!password) {
+    passwordInput.classList.remove(`good`);
+    passwordInput.classList.remove(`bad`);
+  }
+}
+
+function helloHandler() {
+  const age = ageInput.value;
+  const comment = commentInput.value;
+
+  h1.textContent = `jusu amzius yra ${age} ir jusu nuomone apie pasauli: ${comment}`;
+}
 
 function handleSubmit(event) {
   event.preventDefault();
