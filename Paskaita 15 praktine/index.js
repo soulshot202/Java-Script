fbcef5bd3ee4c3a04c3489a444c7f33d5910a7ce
Vytautas Berzvinskas <misionierius@gmail.com>
2023-08-18 14:00:50 +0300

    bagtas darbas

--- Paskaita 15 praktine/index.js
+++ Paskaita 15 praktine/index.js
@@ -44,6 +44,7 @@ function atbulai(zodis) {
   for (let i = zodis.length - 1; i >= 0; i--) {
     atbulas += zodis[i];
   }
+  atbulas = atbulas.toLowerCase();
   return atbulas;
 }
 console.log("uzduotis 6", atbulai("Zigmas"));
@@ -52,14 +53,30 @@ console.log("uzduotis 6", atbulai("Zigmas"));
 //  7. Parašykite funkciją, kuri paims array skaičių ir
 // grąžins didžiausią neigiamą skaičių.
 // pvz: [-1, -100, -5, 10, 0, 11] -> "-1"
+//?????????????????????????????????????
+const skaiciai = [-1, -100, -5, 10, 0, 11];
+const skaiciaiNeigiami = skaiciai.filter((x) => x < 0).sort((a, b) => b - a);
+
+console.log("uzduotis 7", skaiciaiNeigiami[0]);
 
 //  8. Sukurkite funkciją, kuri paims skaičių parametrą
 //ir sugeneruos array su tiek random skaičių (nuo 1 - 10)),
 //kiek parametre nurodyta.T.y. "3" => [1, 6, 3].
+function randomSk(x) {
+  const arr = [];
+  for (let i = 0; i < x; i++) {
+    arr.push(Math.floor(Math.random() * 10) + 1);
+  }
+  return arr;
+}
+console.log("uzduotis 8", randomSk(10));
 
 //  9. Sukurkite arrow funkciją, kuri paimtų du skaičius
 // ir grąžintų boolean value ar skaičių suma didesnė
 // už 100 ar mažesnė (ir lygi).Pvz.: fn(10, 50) -> true
+
+const palyg = (sk1, sk2) => sk1 + sk2 <= 100;
+console.log("uzduotis 9", palyg(70, 60));
 
 // 10.  Parašykite funkciją, kuri paimtų array
 //(susideda iš objektų su vardu ir amžium) bei
@@ -70,11 +87,63 @@ console.log("uzduotis 6", atbulai("Zigmas"));
 //[{name: „Kasparas“, age: 20}, {name: „Alfredas“, age: 25},
 // {name: „Jonas“, age: 25}]
 
+const duomenys = [
+  { vardas: "Tadas", amzius: 26 },
+  { vardas: "Maryte", amzius: 14 },
+  { vardas: "Jurgita", amzius: 18 },
+  { vardas: "Zigmas", amzius: 25 },
+  { vardas: "Vaisvildas", amzius: 25 },
+  { vardas: "Bronele", amzius: 86 },
+  { vardas: "Stefa", amzius: 86 },
+  { vardas: "Antanas", amzius: 86 },
+];
+
+function rikiuoti(v) {
+  const surikiuota = v.sort((a, b) =>
+    a.amzius === b.amzius ? a.vardas > b.vardas : a.amzius - b.amzius
+  );
+
+  return surikiuota;
+}
+
+console.log("uzduotis 10", rikiuoti(duomenys));
 //  11. Parašykite funkciją, į kurią padavus datą, pasakys
 //ar išeiginė ar ne (visos išeiginės turi būti saugomos array.)
 // Pvz.: fn(new Date(2020, 12, 25)) -> true
+const iseigines = [
+  "2023-05-05",
+  "2023-09-01",
+  "2023-12-24",
+  "2023-10-01",
+  "2024-01-01",
+];
+const data = new Date().toLocaleDateString();
+function arIseigine(date) {
+  if (iseigines.includes(date.toLocaleDateString())) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+console.log("uzduotis 11", arIseigine(new Date("2024-01-01")));
 
 //  12. Sukurkite funkciją, kuri kaip parametrą
 //gaus array su skaičiais. Funkcija turės grąžinti
 // mažiausią trūkstamą skaičių iš array.
 // Pvz. Paduodu: [1, 2, 4, 5]; Grąžina: 3
+const skaiciuArr = [1, 2, 3, 5, 6, 7, 9, 10, 11, 12];
+const trukstamas = tikr(skaiciuArr);
+function tikr(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] !== i + 1) {
+      return i + 1;
+    }
+  }
+}
+
+console.log("uzduotis 12", trukstamas);
+
+// const missingNumber = (array) =>
+//   array.find((x, i) => x + 1 !== array[i + 1]) + 1;
+// console.log(missingNumber(skaiciuArr));
