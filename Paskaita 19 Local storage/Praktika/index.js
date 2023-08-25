2b000d52a1197a082f5d000248692f234fe76edd
Vytautas Berzvinskas <misionierius@gmail.com>
2023-08-25 15:53:03 +0300

    baigtos 2 uzduotys.pradeta 3ia

--- Paskaita 19 Local storage/Praktika/index.js
+++ Paskaita 19 Local storage/Praktika/index.js
@@ -9,15 +9,14 @@ const formSubmit = document.querySelector("#forma");
 const submitButton = document.querySelector("#submit");
 const output = document.querySelector("#output");
 const nameOutput = document.querySelector("#name1");
-// document
-//   .querySelector("#forma")
-//   .addEventListener("submit", (e) => e.preventDefault());
+document
+  .querySelector("#forma")
+  .addEventListener("submit", (e) => e.preventDefault());
 formSubmit.addEventListener("submit", displayName);
 const deleteButton = document.querySelector("#deleteName");
 
 const vardasStorage = localStorage.getItem("name");
-function displayName(e) {
-  e.preventDefault();
+function displayName() {
   const vardas = vardasInput.value;
 
   if (vardasStorage !== "" && vardasStorage) {
@@ -95,16 +94,88 @@ displayName();
   }); 
   */
 
-
-
 // 2.  localStorage, įrašykite savo vardą, pavardę,
 //aprašymą, ir nuorodą į profilio nuotrauką. Informaciją
 //įrašykite pirmą kartą užkrovus puslapį, o vėliau ją atvaizduokite
 
-
+const zmogus1 = {
+  vardas: "Vytautas",
+  pavarde: "Berzvinskas",
+  aprasymas: "Linksmas zmogus",
+  foto: "https://hips.hearstapps.com/hmg-prod/images/beautiful-smooth-haired-red-cat-lies-on-the-sofa-royalty-free-image-1678488026.jpg?crop=0.668xw:1.00xh;0.119xw,0&resize=1200:*",
+};
+const savedPeople = localStorage.getItem("zmogus1");
+const uzd2Res = document.querySelector("#uzd2");
+const delZmogButton = document.querySelector("#delZmogus");
+function zmogusLoad(obj) {
+  if (savedPeople && savedPeople !== "") {
+    delZmogButton.classList.remove("hidden");
+    const zmogus = JSON.parse(savedPeople);
+    const vardas = zmogus.vardas;
+    const pavarde = zmogus.pavarde;
+    const aprasymas = zmogus.aprasymas;
+    const fotoUrl = zmogus.foto;
+    const foto = document.createElement("img");
+    foto.style.maxWidth = "300px";
+    foto.src = fotoUrl;
+    const p1 = document.createElement("p");
+    const p2 = document.createElement("p");
+    const p3 = document.createElement("p");
+    const p4 = document.createElement("p");
+    p1.textContent = `Vardas: ${vardas}`;
+    p2.textContent = `Pavarde: ${pavarde}`;
+    p3.textContent = `Apie: ${aprasymas}`;
+    p4.textContent = `${Math.random()}`;
+    uzd2Res.append(p1, p2, p3, p4, foto);
+  } else {
+    localStorage.setItem("zmogus1", JSON.stringify(obj));
+    delZmogButton.classList.add("hidden");
+  }
+}
+zmogusLoad(zmogus1);
+delZmogButton.addEventListener("click", () =>
+  localStorage.removeItem("zmogus1")
+);
 
 // 3. Sukurkite puslapį, kuriame būtų forma su vienu input - fullName.
 //Įvedus vardą ir pavardę, juos padalina į dvi dalis (name ir surname).
 // Vardą ir pavardę įdeda į objektą, o objektą - į array. Šį array
 //išsaugo localStorage. Po forma sukurkite lentelę joje atvaizduokite
 //informaciją iš localStorage
+
+const fullNameInput = document.querySelector("#fullName");
+fullNameInput.addEventListener("blur", splitName);
+
+function splitName() {
+  const vardas1 = new vardas(fullNameInput.value.trim());
+
+  vardas1.takeNameToTable();
+}
+class vardas {
+  constructor(fullName) {
+    this.fullName = fullName;
+    const nameArr = fullName.split(" ");
+    const names = nameArr
+      .filter((e) => {
+        if (e !== "") {
+          return e;
+        }
+      })
+      .map((el) => el.charAt(0).toUpperCase() + el.slice(1).toLowerCase());
+
+    this.firstName = names[0];
+    this.lastName = names[1];
+    delete this.fullName;
+  }
+
+  takeNameToTable() {
+    const table = document.querySelector("tbody");
+    const tr = document.createElement("tr");
+    const tdLastName = document.createElement("td");
+    const tdname = document.createElement("td");
+    tdname.textContent = this.firstName;
+    tdLastName.textContent = this.lastName;
+    tr.append(tdname, tdLastName);
+    table.append(tr);
+  }
+}
